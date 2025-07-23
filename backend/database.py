86a8a1521c9423e2aa18e9a88cb6358e06088ea8
woonjincho <woonjincho@naver.com>
2025-07-23 17:37:39 +0900

    frontend 추가

--- backend/database.py
+++ backend/database.py
@@ -3,6 +3,6 @@
 from databases import Database
 import sqlalchemy
 
-DATABASE_URL = "mysql+pymysql://yubin:1234@localhost/testdb"
+DATABASE_URL = "mysql+pymysql://root:1234@localhost/testdb"
 
 database = Database(DATABASE_URL)
