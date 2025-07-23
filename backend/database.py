9edf8a60234fc4908645e8dee925092804dea31e
0nam1 <damonte@naver.com>
2025-07-23 22:25:37 +0900

    databases 파일 변경

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,12 @@
 # database.py
-
 from databases import Database
 import sqlalchemy
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "mysql+pymysql://yubin:1234@localhost/testdb"
+load_dotenv()
+dbpassword = os.getenv("MYSQLPW")
+
+DATABASE_URL = f"mysql+pymysql://7aiteam3:{dbpassword}@20.196.112.223:3306/gympt_db"
 
 database = Database(DATABASE_URL)
